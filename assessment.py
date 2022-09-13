f7732067a7a4969f42e2d8658716581780c662c3
Watermelon Man <71874748+YAO-Shuyang@users.noreply.github.com>
2022-09-13 23:44:03 +0800

    A score generator for assessment

--- assessment.py
+++ assessment.py
@@ -1,12 +1,13 @@
 import numpy as np
 import pandas as pd
+import os
 
 n = 20 # number, 班里有多少学生
 col = 5 # column, 有几项需要随机（学习、思想等）
 
 p1 = 0.2 # (5, first 20%)
-p2 = 0.2 # (4, second 40%)
-p3 = 0.6 # (3, last 40%)
+p2 = 0.2 # (4, second 20%)
+p3 = 1 - p1 - p2 # (3, last 60%)
 
 scores = np.zeros((n,col+1))
 p = np.concatenate([np.repeat(5,int(n*p1)), np.repeat(4,int(n*p2)), np.repeat(3,n - int(n*(p1+p2)))])
@@ -16,6 +17,7 @@ for c in range(col):
 scores[:,col] = np.nansum(scores, axis=1)
 
 data = pd.DataFrame(scores)
-data.to_excel(r'D:\YAO_Shuyang\Students work\assessment.xlsx', sheet_name = '2022')
+path = "D:"       # Directory, 保存的文件夹路径
+data.to_excel(os.path.join(path, 'assessment.xlsx'), sheet_name = '2022')
 
 print(scores)
